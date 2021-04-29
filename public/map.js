b481c4009b26e733f77d4121c4ac5c565297b02a
James <4dunsj71@solent.ac.uk>
2021-04-29 19:18:21 +0100

    add on map click gone wrong

--- public/map.js
+++ public/map.js
@@ -36,8 +36,50 @@ map.on('click', function(ev) {
     console.log(lat);
     console.log(lon);
     const marker = L.marker(location).addTo(map);
-    marker.bindPopup("").openPopup();
-    
+    marker.bindPopup(`
+    <form>
+        <div class ="form-group">
+            <label for="nameIn">Name</label>
+            <input type="text" class="form-control" id="nameIn">
+        </div>
+        <div class ="form-group">
+            <label for="typeIn">type</label>
+            <input type="text" class="form-control" id="typeIn">
+        </div>
+        <div class ="form-group">
+            <label for="countryIn">Country</label>
+            <input type="text" class="form-control" id="countryIn">
+        </div>
+        <div class ="form-group">
+            <label for="regionIn">Region</label>
+            <input type="text" class="form-control" id="regionIn">
+        </div>
+        <div class ="form-group">
+            <label for="descriptionIn">Description</label>
+            <input type="text" class="form-control" id="descriptionIn">
+        </div>
+        <p class="d-none" id="latIn">${lat}</p>
+        <p class="d-none" id="lonIn">${lon}</p>
+        <script type="text/javascript">
+        let nameIn = document.getElementById("nameIn").value;
+        let typeIn = document.getElementById("typeIn").value;
+        let countryIn = document.getElementById("countryIn").value;
+        let regionIn = document.getElementById("regionIn").value;
+        let latIn = document.getElementById("latIn").value;
+        let lonIn = document.getElementById("lonIn").value;
+        let description = document.getElementById("descriptionIn").value;
+        </script>
+        <input type="button"  id="subDetails" value="Submit" onclick="newpoi(
+            nameIn,
+            typeIn,
+            countryIn,
+            regionIn,
+            latIn,
+            lonIn,
+            descriptionIn)"></input>
+    </form>
+`).openPopup();
+
 });
 
 
--- public/poionclick.js
+++ public/poionclick.js
@@ -1,8 +1,30 @@
-map.on("click", e =>{
-    lat = e.latlng.lat;
-    lon = e.latlng.lon;
-    location = [lat,lon];
-    const marker = L.marker(location);
-    marker.bindPopup("gay").openPopup();
+async function newpoi(name,type,country,region,lat,lon,description){
+    try{
+        const poi ={
+            name: name, 
+            type: type, 
+            country: country, 
+            region: region, 
+            lon: lon, 
+            lat: lat, 
+            description: description, 
+            recommendations: 0
+        };
+        console.log(poi);
+
+        const response = await fetch('http://localhost:3000/newpoi',{
+            method: 'POST',
+            headers: {'Content-Type' : 'application/json'
+            },
+            body: JSON.stringify(poi)
+
+        });
+        console.log(poi);
+    }catch(e){
+        alert(`Error: ${e}`);
+        console.log(poi);
+    }
+}
+
     
-});
+    
